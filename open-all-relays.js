60e7787c813d062acfcbeb7114ec0a2c8231597f
Ian Alves <igaps.eng17@uea.edu.br>
2020-07-21 15:08:37 -0400

    update

--- open-all-relays.js
+++ open-all-relays.js
@@ -5,6 +5,7 @@ module.exports = function(RED) {
 
     function openAllRelaysNode(config) {
         RED.nodes.createNode(this, config);
+        this.slot = config.slot;
         // this.websocket = config.websocket;
         // this.websocketConfig = RED.nodes.getNode(this.websocket);
         var node = this
@@ -19,20 +20,39 @@ module.exports = function(RED) {
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
             var command = {
-                type: "multimeter_modular_V1.0",
-                slot: 1,
+                type: "multimeter_modular_V1_0",
+                slot: parseInt(node.slot),
                 method: "open_all_relays",
+                get_output: {},
+                compare: {}
                 // couple_channel: parseInt(node.channel_number),
                 // AC_mode: node.AC_mode ,
                 // scale: parseInt(node.scale) 
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
             // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
             // msg.payload = command
+            console.log(command)
             send(msg)
         });
     }
