e5ac61b6c3ec228a5841a86f29b1e2b4f45d4069
Ian Alves <ian.alves21@gmail.com>
2020-07-29 20:45:56 -0400

    update

--- open-all-relays.js
+++ open-all-relays.js
@@ -6,14 +6,36 @@ module.exports = function(RED) {
     function openAllRelaysNode(config) {
         RED.nodes.createNode(this, config);
         this.slot = config.slot;
-        // this.websocket = config.websocket;
-        // this.websocketConfig = RED.nodes.getNode(this.websocket);
+
+        
+        this.qtdAll = config.qtdAll;
+        this.slot_n=[]; 
+        this.slot_n.push(config.slot1);
+        this.slot_n.push(config.slot2) ;
+        this.slot_n.push(config.slot3) ;
+        this.slot_n.push(config.slot4) ;
+        this.slot_n.push(config.slot5) ;
+        this.slot_n.push(config.slot6) ;
+        this.slot_n.push(config.slot7) ;
+        this.slot_n.push(config.slot8) ;
+        this.slot_n.push(config.slot9) ;
+        this.slot_n.push(config.slot10) ;
+        this.slot_n.push(config.slot11) ;
+        this.slot_n.push(config.slot12) ;
+        this.slot_n.push(config.slot13) ;
+        this.slot_n.push(config.slot14) ;
+        this.slot_n.push(config.slot15) ;
+        this.slot_n.push(config.slot16) ;
+        this.slot_n.push(config.slot17) ;
+        this.slot_n.push(config.slot18) ;
+        this.slot_n.push(config.slot19) ;
+        this.slot_n.push(config.slot20) ;
+        this.slot_n.push(config.slot21) ;
+        this.slot_n.push(config.slot22) ;
+        this.slot_n.push(config.slot23) ;
+        this.slot_n.push(config.slot24) ;
         var node = this
-        // this.mapeamento = config.mapeamento
-        // node.channel_number = config.channel_number
-        // node.AC_mode = config.AC_mode === "true" ? true : false,
-        // node.scale = config.scale
-        // mapeamentoNode = RED.nodes.getNode(this.mapeamento);
+        
         
         node.on('input', function(msg, send, done) {
             var globalContext = node.context().global;
@@ -25,74 +47,74 @@ module.exports = function(RED) {
                 method: "open_all_relays",
                 get_output: {},
                 compare: {}
-                // couple_channel: parseInt(node.channel_number),
-                // AC_mode: node.AC_mode ,
-                // scale: parseInt(node.scale) 
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
             if(!(slot === "begin" || slot === "end")){
                 if(currentMode == "test"){
                     file.slots[slot].jig_test.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[slot].jig_test.push(
+                            {
+                                type: "multimeter_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                method: "open_all_relays",
+                                get_output: {},
+                                compare: {}
+                            }
+                        );
+                    }
                 }
                 else{
                     file.slots[slot].jig_error.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[slot].jig_error.push(
+                            {
+                                type: "multimeter_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                method: "open_all_relays",
+                                get_output: {},
+                                compare: {}
+                            }
+                        );
+                    }
                 }
             }
             else{
                 if(slot === "begin"){
                     file.slots[0].jig_test.push(command);
-                    // file.begin.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[0].jig_test.push(
+                            {
+                                type: "multimeter_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                method: "open_all_relays",
+                                get_output: {},
+                                compare: {}
+                            }
+                        );
+                    }
                 }
                 else{
                     file.slots[3].jig_test.push(command);
-                    // file.end.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[3].jig_test.push(
+                            {
+                                type: "multimeter_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                method: "open_all_relays",
+                                get_output: {},
+                                compare: {}
+                            }
+                        );
+                    }
                 }
             }
+
             globalContext.set("exportFile", file);
-            // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
-            // msg.payload = command
             console.log(command)
             send(msg)
         });
     }
     RED.nodes.registerType("open-all-relays", openAllRelaysNode);
-
-    // RED.httpAdmin.get("/getVoltageDiff",function(req,res) {
-    //     console.log(mapeamentoNode)
-    //     if(mapeamentoNode){
-    //         res.json([
-    //             {value:mapeamentoNode.valuePort1, label: mapeamentoNode.labelPort1, hasValue:false},
-    //             {value:mapeamentoNode.valuePort2, label: mapeamentoNode.labelPort2, hasValue:false},
-    //             {value:mapeamentoNode.valuePort3, label: mapeamentoNode.labelPort3, hasValue:false},
-    //             {value:mapeamentoNode.valuePort4, label: mapeamentoNode.labelPort4, hasValue:false},
-    //             {value:mapeamentoNode.valuePort5, label: mapeamentoNode.labelPort5, hasValue:false},
-    //             {value:mapeamentoNode.valuePort6, label: mapeamentoNode.labelPort6, hasValue:false},
-    //             {value:mapeamentoNode.valuePort7, label: mapeamentoNode.labelPort7, hasValue:false},
-    //             {value:mapeamentoNode.valuePort8, label: mapeamentoNode.labelPort8, hasValue:false},
-    //             {value:mapeamentoNode.valuePort9, label: mapeamentoNode.labelPort9, hasValue:false},
-    //             {value:mapeamentoNode.valuePort10, label: mapeamentoNode.labelPort10, hasValue:false},
-    //             {value:mapeamentoNode.valuePort11, label: mapeamentoNode.labelPort11, hasValue:false},
-    //             {value:mapeamentoNode.valuePort12, label: mapeamentoNode.labelPort12, hasValue:false},
-    //             {value:mapeamentoNode.valuePort13, label: mapeamentoNode.labelPort13, hasValue:false},
-    //         ])
-    //     }
-    //     else{
-    //         res.json([
-    //             {label:"A1 | B1", value: "0", hasValue:false},
-    //             {label:"A2 | B2", value: "1", hasValue:false},
-    //             {label:"A3 | B3", value: "2", hasValue:false},
-    //             {label:"A4 | B4", value: "3", hasValue:false},
-    //             {label:"A5 | B5", value: "4", hasValue:false},
-    //             {label:"A6 | B6", value: "5", hasValue:false},
-    //             {label:"A7 | B7", value: "6", hasValue:false},
-    //             {label:"A8 | B8", value: "7", hasValue:false},
-    //             {label:"A9 | B9", value: "8", hasValue:false},
-    //             {label:"A10 | B10", value: "9", hasValue:false},
-    //             {label:"A11 | B11", value: "10", hasValue:false},
-    //             {label:"APW | BPW", value: "11", hasValue:false},
-    //             {label:"AMX | BMX", value: "12", hasValue:false},
-    //         ])
-    //     }
-    // });
 }
